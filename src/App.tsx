53425adfb0735e2bdfc290a5ab50f8f5795fbdd6
Krisna Lach <lach.krisna@gmail.com>
2023-04-04 15:32:55 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import SideBar from './components/SideBar'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import SideBar from "./components/SideBar";
+import NavBar from "./components/NavBar";
 
 //import './App.css'
 
 function App() {
-
   return (
     <div>
-      
+      <NavBar />
     </div>
   );
 }
 
-export default App
+export default App;
