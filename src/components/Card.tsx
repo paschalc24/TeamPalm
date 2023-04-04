62597ac9680bc5aa722a67998324ae26f17b3b0a
Krisna Lach <lach.krisna@gmail.com>
2023-04-04 15:28:16 -0400

    Create Card.tsx

---
+++ src/components/Card.tsx
@@ -0,0 +1,12 @@
+import React, { ReactNode } from "react";
+
+interface Props {
+  data: ReactNode;
+  header: string;
+}
+
+const Card = () => {
+  return <div>Card</div>;
+};
+
+export default Card;
