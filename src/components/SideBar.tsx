152bb080a637047579434c16271010f425f9b016
ZMTakriti <71720717+ZMTakriti@users.noreply.github.com>
2023-04-11 06:39:02 -0400

    Update sidebar code

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,138 +1,81 @@
-import { AiFillHome } from "react-icons/ai";
-import { AiFillBell } from "react-icons/ai";
-import React, { useState } from "react";
-import { PRIMARY_COLOR } from "../configs/colors";
-import CourseIcon from './CourseIcon'
+import React from "react";
+import styled from "styled-components";
+import CourseButton from "./CourseButton";
+import userIcon from "../assets/user-icon.png";
+import settingsIcon from "../assets/settings-icon.jpg";
+
+interface SidebarProps {
+  courses: { imageSrc: string; tooltipText: string; onClick: () => void }[];
+  username: string;
+}
+
+const SidebarWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  width: 80px;
+  background-color: ${({ theme }) => theme.colors.darkBlue};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const CourseButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 70px
+`;
 
 
-function SideBar() {
+const ProfileSection = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
-  const [touched, setTouched] = useState(false);
-  const className = touched ? { touchedButton } : { dynamicButton };
+const ProfilePic = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  margin-right: 10px;
+`;
 
-  const toggleTouched = () => {
-    setTouched((prevState) => !prevState.touched);
-    className = { touchedButton };
-  };
+const ProfileInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
-  const handleMouseUp = () => {
-    // Handle smooth animation when clicking without holding
-    setTimeout(() => {
-      setTouched(false);
-      className = { dynamicButton };
-    }, 150);
-  };
-  const homeClick = () => {
-    setShowHome(true);
-    return (
-      <div className="App" style={{ backgroundColor: "blue", display: "flex" }}>
-        {/* {showCreateEvent ? (
-          <CreateEventScreen toggleCreateEvent={toggleCreateEvent} />
-        ) : (
-          <>
-            {" "}
-            <CreateButton handleClick={toggleCreateEvent} />{" "}
-          </>
-        )} */}
-        <SideBar rsoData={rsoData} />
-        {showHome ? <Home/> : <OneRSO currCard={currCard} setCard={setCard} />}
-      </div>
-    );
-  };
+const Name = styled.span`
+  font-size: 16px;
+  font-weight: bold;
+`;
 
+const SettingsIcon = styled.img`
+  width: 20px;
+  height: 20px;
+  margin-top: 5px;
+`;
+
+const Sidebar: React.FC<SidebarProps> = ({ courses, username }) => {
   return (
-    <div style={sidebarStyle}>
-      <button style={homeButton} onClick={homeClick}>
-        <AiFillHome style={iconStyle} />
-      </button>
-
-      {/* <button
-        onClick = {homeClick}
-        style = {className}
-        onMouseDown={toggleTouched}
-        onMouseUp={handleMouseUp}
-        >
-        <AiFillHome style = {iconStyle}/>     
-      </button> */}
-
-      <hr
-        style={{
-          background: "white",
-          color: "white",
-          borderColor: "white",
-          height: "3px",
-          width: "10vw",
-          transparency: "0",
-        }}
-      />
-      {rsoData.map((rso) => (
-        <div style={{ marginBottom: "10px" }}>
-          <CourseIcon img={rso.img} handleClick={props.showRSO} />
-        </div>
-      ))}
-      {/* <RSOIcon img = {msa}/> */}
-      <AiFillBell style={bellStyle}></AiFillBell>
-    </div>
+    <SidebarWrapper>
+      <CourseButtonContainer>
+        {courses.map((course) => (
+          <CourseButton {...course} />
+        ))}
+      </CourseButtonContainer>
+      <ProfileSection>
+        <ProfilePic src="profile-pic.png" />
+        <ProfileInfo>
+          username
+          <SettingsIcon src="settings-icon.png" />
+        </ProfileInfo>
+      </ProfileSection>
+    </SidebarWrapper>
   );
 };
 
-const sidebarStyle = {
-  display: "flex",
-  height: "100vh",
-  width: "15vw",
-  background: PRIMARY_COLOR,
-  flexDirection: "column",
-  alignItems: "center",
-};
-
-const iconStyle = {
-  height: "4vh",
-  width: "11vw",
-  color: "white",
-};
-
-const bellStyle = {
-  height: "4vh",
-  width: "11vw",
-  color: "white",
-  flex: 1,
-  flexDirection: "column",
-  justifyContent: "bottom",
-  marginBottom: "-50vh",
-};
-
-const homeButton = {
-  marginTop: "1vh",
-  height: "6vh",
-  width: "15vw",
-  color: "white",
-  backgroundColor: PRIMARY_COLOR,
-  borderColor: "transparent",
-};
-
-const dynamicButton = {
-  height: "6vh",
-  width: "15vw",
-  marginTop: "100vh",
-  borderColor: "transparent",
-  background: PRIMARY_COLOR,
-  cursor: "pointer",
-  color: "white",
-  iconColor: "white",
-
-  opacity: "1",
-  transition: "opacity 300ms ease",
-};
-
-const touchedButton = {
-  marginTop: "100vh",
-  background: PRIMARY_COLOR,
-  cursor: "pointer",
-  height: "6vh",
-  width: "15vw",
-  color: "white",
-
-  opacity: "0.5",
-};
-
-export default SideBar
+export default Sidebar;
