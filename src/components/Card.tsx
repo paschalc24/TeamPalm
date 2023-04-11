1b0a5a720465954cd9c99ec4f7ac9e550f704f4c
Krisna Lach <lach.krisna@gmail.com>
2023-04-11 15:50:10 -0400

    Update Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const Card = () => {
+interface CardProps {
+  heading: string;
+}
+
+const Card = ({ heading }: CardProps) => {
   return (
     <div className="card shadow">
       <div className="card-body">
-        <h5 className="card-title">Title</h5>
+        <h5 className="card-title">{heading}</h5>
         <h6 className="card-subtitle mb-2 text-muted">Subtitle</h6>
         <p className="card-text">Text</p>
       </div>
