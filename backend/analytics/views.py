3b4ee9496087dc3703b56dbc0bc80e04a47b00ef
Gazi10 <azwadgazi@gmail.com>
2023-04-11 18:05:11 -0400

    Add GET all authors  endpoint

--- backend/analytics/views.py
+++ backend/analytics/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Author
+from .serializers import AuthorSerializer
 
-# Create your views here.
+class AnalyticsApiView(APIView):
+    # 1. List all Authors
+    def get(self, request):
+      authors = Author.objects.all()
+      serializer = AuthorSerializer(authors, many=True)
+      return Response(serializer.data, status=status.HTTP_200_OK)
