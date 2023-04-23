5048ea677164c8572eb835a546b798037d43fe60
ShriramG24 <shrirambhavin@gmail.com>
2023-04-23 15:04:17 -0400

    Drafted getPostsByTimeFrame

--- backend/analytics/views.py
+++ backend/analytics/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Author, Post
 from .serializers import AuthorSerializer, PostSerializer
+from datetime import datetime
 
 class AnalyticsApiAllAuthorsView(APIView):
     # List all Authors
@@ -42,4 +43,21 @@ class AnalyticsPostByAuthorApiView(APIView):
             status=status.HTTP_400_BAD_REQUEST
         )
     serializer = PostSerializer(Post_instance, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+class AnalyticsPostByTimeframeApiView(APIView):
+  # Retrieves the post within given time-frame
+  def get(self, request, start_time, end_time):
+    time_format = "%Y-%m-%dT%H:%M:%S.%fZ"
+    str_to_time = lambda time_str: datetime.strptime(time_str, time_format)
+
+    start_time, end_time = str_to_time(start_time), str_to_time(end_time)
+    posts = set()
+    for post in Post.objects.all():
+      publish_time = str_to_time(post.publishedAt)
+      if start_time <= publish_time <= end_time:
+        posts.add(post.number)
+
+    response = Post.objects.filter(number__in=posts)
+    serializer = PostSerializer(response, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
