d325f77eac71ed5134daf782dcda4266561a1b9a
ZMTakriti <71720717+ZMTakriti@users.noreply.github.com>
2023-05-04 14:05:10 -0400

    Update OverlayHub for mode switching

--- src/components/OverlayHub.tsx
+++ src/components/OverlayHub.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import { useState } from "react";
 import SideBar from "./SideBar"
 import Overlay from "./Overlay"
+import NavBar from "./NavBar"
 
 
 interface OverlayHubProps {
@@ -14,18 +15,37 @@ interface OverlayHubProps {
   
   const OverlayHub: FC<OverlayHubProps> = ({ courses }) => {
     const [selectedCourse, setSelectedCourse] = useState("");
+    const [selectedMode, setSelectedMode] = useState("home");
+    //const [selectedMode, setSelectedMode] = useState("");
+
+
+    const modeHandler = (newMode: string) => {
+        setSelectedMode(newMode);
+    }
+
     return (
-        <div style={{ display: 'flex' }}>
-            <div style={{ width: '250px' }}>
-                <SideBar courses={courses} username="Matthew Rattigan"
-                selectedCourse={selectedCourse} setSelectedCourse={setSelectedCourse} />
-            </div>
-            <div style={{flex:1}}>
-                {courses.map((course, index) => (
-                    <Overlay key={index} course={course.tooltipText} activeCourse={selectedCourse}/>
-                ))}
+        <div>
+            {selectedCourse != "" ?
+                (courses.map((course, index) => (
+                    <NavBar key={index} course={course.tooltipText} activeCourse={selectedCourse} activeMode={selectedMode} setSelectedMode={setSelectedMode}/>
+                )))
+                :
+                <NavBar course={""} activeCourse={""} activeMode={""} setSelectedMode={setSelectedMode}/>
+            }
+            
+            <div style={{ display: 'flex' }}>
+                <div style={{ width: '250px' }}>
+                    <SideBar courses={courses} username="Matthew Rattigan"
+                    selectedCourse={selectedCourse} setSelectedCourse={setSelectedCourse} />
+                </div>
+                <div style={{flex:1}}>
+                    {courses.map((course, index) => (
+                        <Overlay key={index} course={course.tooltipText} activeCourse={selectedCourse} activeMode={selectedMode}/>
+                    ))}
+                </div>
             </div>
         </div>
+        
     );
   };
   
