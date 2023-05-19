af8bc556f246b222c22b38353865bd65cd10a189
Krisna Lach <lach.krisna@gmail.com>
2023-05-18 22:30:01 -0400

    Add light purple box shadow to Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,8 +9,12 @@ interface CardProps {
 const Card = ({ heading, children }: CardProps) => {
   return (
     <div
-      className="card shadow"
-      style={{ borderRadius: "15px", backgroundColor: "#FFFFFF" }}
+      className="card"
+      style={{
+        borderRadius: "15px",
+        backgroundColor: "#FFFFFF",
+        boxShadow: "0px 10px 20px rgba(153, 37, 190, 0.1)",
+      }}
     >
       <div className="card-body">
         <h5 className="card-title">{heading}</h5>
